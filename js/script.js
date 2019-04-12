93399f005144c2b0063e56d429e1ec53ad78e747
Brandon Aaronson <45774261+brandonaaronson@users.noreply.github.com>
2019-04-12 14:14:40 -0700

    Made JS search function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-$('#gallery').lightGallery(function({
-    $('div').index();
-    console.log(index);
-}));
+$('#srchInput').on('keyup', function() {
+
+    const input  = $('#srchInput').val().toUpperCase();
+    console.log(input);
+    const aRef   = $('a[href*="photos"]');
+    const imgRef = $('img[src*="photos"]');
+    for ( i=0; i < aRef.length; i += 1) {
+        const caption = $(aRef[i]).attr('title').toUpperCase();
+        const $attr = $(imgRef[i]).attr('alt').toUpperCase();
+        if (caption.indexOf(input) > -1 || $attr.indexOf(input) > -1) {
+            aRef[i].style.display = '';
+        } else {
+            aRef[i].style.display = 'none';
+        }
+    }
+}); 
