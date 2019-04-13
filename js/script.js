fcf7702a05162cb77cea3fec2318af940b81e9b7
Brandon Aaronson <45774261+brandonaaronson@users.noreply.github.com>
2019-04-13 13:17:05 -0700

    added more specific syntax targeting classes for my JQuery

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 $('#srchInput').on('keyup', function() {
 
     const $input  = $('#srchInput').val().toUpperCase();
-    const aRef   = $('.gallery a');
-    const imgRef = $('.gallery img');
-    for ( i=0; i < aRef.length; i += 1) {
-        const caption = $(aRef[i]).attr('title').toUpperCase();
-        const $attr = $(imgRef[i]).attr('alt').toUpperCase();
+    const $aRef   = $('.aref');
+    const $imgRef = $('.isrc');
+    for ( i=0; i < $aRef.length; i += 1) {
+        const caption = $($aRef[i]).attr('title').toUpperCase();
+        const $attr = $($imgRef[i]).attr('alt').toUpperCase();
         if (caption.indexOf($input) > -1 || $attr.indexOf($input) > -1) {
-            aRef[i].style.display = '';
+            $aRef[i].style.display = '';
         } else {
-            aRef[i].style.display = 'none';
-            aRef[i].style.float = 'right';
+            $aRef[i].style.display = 'none';
+            $aRef[i].style.float = 'right';
         }
-    } console.log(aRef);
-}); 
+    } console.log($aRef);
+});
