532077d26596ee4302675524534476612aa6bdca
Brandon Aaronson <45774261+brandonaaronson@users.noreply.github.com>
2019-04-13 10:18:35 -0700

    Updated JS and added MQ for mobile

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 $('#srchInput').on('keyup', function() {
 
-    const input  = $('#srchInput').val().toUpperCase();
-    const aRef   = $('a[href*="photos"]');
-    const imgRef = $('img[src*="photos"]');
+    const $input  = $('#srchInput').val().toUpperCase();
+    const aRef   = $('.gallery a');
+    const imgRef = $('.gallery img');
     for ( i=0; i < aRef.length; i += 1) {
         const caption = $(aRef[i]).attr('title').toUpperCase();
         const $attr = $(imgRef[i]).attr('alt').toUpperCase();
-        if (caption.indexOf(input) > -1 || $attr.indexOf(input) > -1) {
+        if (caption.indexOf($input) > -1 || $attr.indexOf($input) > -1) {
             aRef[i].style.display = '';
         } else {
             aRef[i].style.display = 'none';
             aRef[i].style.float = 'right';
         }
-    }console.log(input);
+    } console.log(aRef);
 }); 
